07ebe318f13375ac62959192f0497a12b7d3542a
maromaroXD <amine-fb-fb@hotmail.fr>
2021-01-24 23:21:37 +0100

    Session and Okta authentication strategies.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,8 +1,20 @@
 "use strict";
 
-const sql = require("./sql");
 
+const sql = require("./sql");
+const auth = require("./auth");
+
+const ejs = require("ejs");
+const inert = require("@hapi/inert");
+const vision = require("@hapi/vision");
 
 module.exports.register = async server => {
-  await server.register( sql );
+  await server.register( [ auth , inert, vision, sql ] );
+
+  server.views({
+    engines: { ejs },
+    relativeTo: __dirname,
+    path: "../templates",
+    layout: true
+  });
 };
